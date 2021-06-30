f4f8ed2af84f07a07c349a26f1efca897052aaa9
dipti.malla <diptimalla7@gmail.com>
2021-06-30 16:25:23 +0530

    add contenteditable

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const newCard= ({id,imageUrl, taskTitle, taskType , taskDescription}) => `
 <div class="col-md-6 col-lg-4" id=${id}>
 <div class="card">
   <div class="card-header d-flex justify-content-end gap-2">
-    <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-    <button type="button" id=${id} class="btn btn-outline-danger" onclick= "deleteCard.apply(this , arguments)"><i class="fas fa-trash" id=${id} onclick= "deleteCard.apply(this , arguments)"></i></button>
+    <button type="button" id=${id} class="btn btn-outline-success" onclick= "editCard.apply(this , arguments)"><i class="fas fa-pencil-alt" id=${id} onclick="editCard()"></i></button>
+  
+    <button type="button" id=${id} class="btn btn-outline-danger" onclick= "deleteCard.apply(this , arguments)"><i class="fas fa-trash" id=${id} onclick= "deleteCard.apply(this , arguments)" id=${id}></i></button>
   </div>
   <img src=${imageUrl} class="card-img-top" alt="...">
   <div class="card-body">
@@ -78,4 +79,26 @@ const deleteCard= (event) => {
 
    return taskContainer.removeChild(
     event.target.parentNode.parentNode.parentNode.parentNode);
+};
+
+const editCard= (event) => {
+  event= window.event;
+  const targetID = event.target.id;
+  const tagName = event.target.tagName;
+
+  let parentElement;
+  if(tagName === "BUTTON"){
+    parentElement = event.target.parentNode.parentNode;
+  }else{
+    parentElement = event.target.parentNode.parentNode.parentNode;
+  }
+  let taskTitle = parentElement.childNodes[5].childNodes[1];
+  let taskDescription =parentElement.childNodes[5].childNodes[3];
+  let taskType =parentElement.childNodes[5].childNodes[5];
+  let submitButton=parentElement.childNodes[7].childNodes[1];
+
+  taskTitle.setAttribute("contenteditable", "true");
+  taskDescription.setAttribute("contenteditable", "true");
+  taskType.setAttribute("contenteditable", "true");
+  submitButton.innerHTML= "Save Changes";
 };
